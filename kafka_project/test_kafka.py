a52cf48a3920b9751a112c0b01d1f13e4ecfe3f2
sersim33 <sersim33@gmail.com>
2025-03-22 12:40:17 +0200

    Moved specific files from venv to kafka_project

---
+++ kafka_project/test_kafka.py
@@ -0,0 +1,12 @@
+from kafka import KafkaProducer
+from configs import kafka_config
+
+producer = KafkaProducer(
+    bootstrap_servers=kafka_config["bootstrap_servers"],
+    security_protocol=kafka_config["security_protocol"],
+    sasl_mechanism=kafka_config["sasl_mechanism"],
+    sasl_plain_username=kafka_config["username"],
+    sasl_plain_password=kafka_config["password"]
+)
+
+print("Kafka producer created successfully!")
